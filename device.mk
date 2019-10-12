3304f49cff109712b47bd58ff522882b61cfecfd
dianlujitao <dianlujitao@lineageos.org>
2019-10-12 10:12:37 +0800

    victara: Enable AOSP bluetooth audio HAL v2

--- device.mk
+++ device.mk
@@ -56,7 +56,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0-impl \
     android.hardware.bluetooth@1.0-service \
-    libbt-vendor
+    libbt-vendor \
+    audio.bluetooth.default \
+    android.hardware.bluetooth.audio@2.0-impl
 
 # Boot animation
 TARGET_BOOTANIMATION_HALF_RES := true
