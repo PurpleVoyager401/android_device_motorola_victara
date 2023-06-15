4540914cdb095642e6d2658c55fd8fb3075618fa
Khalvat-M <mmba313@gmail.com>
2023-06-15 11:20:52 +0330

    victara: Use Aidl drm clearkey

--- device.mk
+++ device.mk
@@ -104,7 +104,7 @@ PRODUCT_PACKAGES += \
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.2.vendor \
-    android.hardware.drm@1.4-service.clearkey
+    android.hardware.drm-service.clearkey
 
 # Gatekeeper
 PRODUCT_PACKAGES += \
