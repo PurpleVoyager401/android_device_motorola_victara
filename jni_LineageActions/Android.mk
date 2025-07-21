2290beeec60d1a04304135ac894fa73322004286
RodrigoAyuso <rodrigo.mazzonetti@gmail.com>
2025-07-21 19:32:07 -0300

    victara: Uptade LineageActions

--- jni_LineageActions/Android.mk
+++ jni_LineageActions/Android.mk
@@ -4,6 +4,6 @@ include $(CLEAR_VARS)
 LOCAL_SRC_FILES := jni_LineageActions.c
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := libjni_LineageActions
-LOCAL_SHARED_LIBRARIES := libcutils liblog
+LOCAL_SHARED_LIBRARIES := libcutils liblog libnativehelper
 LOCAL_HEADER_LIBRARIES := generated_kernel_headers
 include $(BUILD_SHARED_LIBRARY)
