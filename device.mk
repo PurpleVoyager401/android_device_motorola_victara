06541d7a8c83b9ad2ee6173574c8d4e65710cce5
hpnightowl <iamhp2k@gmail.com>
2025-07-21 18:39:07 -0300

    msm8974-common: drop audio.a2dp.default - building from here   https://github.com/LineageOS/android_build/blob/lineage-18.1/target/product/full_base.mk#L35
    Change-Id: Ib71c1abd381468ec3621d60e5ca959f738f51168

--- device.mk
+++ device.mk
@@ -32,7 +32,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.service \
     android.hardware.audio.effect@7.0-impl \
     android.hardware.soundtrigger@2.0-impl \
-    audio.a2dp.default \
     audio.primary.msm8974 \
     audio.r_submix.default \
     audio.usb.default \
