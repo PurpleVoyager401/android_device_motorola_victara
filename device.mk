04d37afc21ada759f3730a0b542cc4e940cdefb7
RodrigoAyuso <rodrigo.mazzonetti@gmail.com>
2025-07-22 20:10:05 -0300

    victara: Use hardware/motorola's common MotoActions

--- device.mk
+++ device.mk
@@ -168,11 +168,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.victara
 
-# LineageActions
-PRODUCT_PACKAGES += \
-    LineageActions \
-    libjni_LineageActions
-
 # LiveDisplay
 PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay@2.0-service-sysfs
@@ -185,6 +180,12 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml
+    
+# MotoActions
+ifeq ($(filter ahannah rhannah,$(TARGET_DEVICE)),)
+PRODUCT_PACKAGES += \
+    MotoActions
+endif
 
 # NFC
 PRODUCT_PACKAGES += \
