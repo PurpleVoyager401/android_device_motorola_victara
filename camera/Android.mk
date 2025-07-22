a02d9297dd982926b128a50283347bfe3cd3e61e
Khalvat-M <mmba313@gmail.com>
2025-07-21 22:57:54 -0300

    msm8974-common: Camrea: Add  more dependecies

--- camera/Android.mk
+++ camera/Android.mk
@@ -1,13 +1,15 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
-LOCAL_C_INCLUDES := system/media/camera/include
+LOCAL_C_INCLUDES := \
+    framework/native/include \
+    system/media/camera/include
 
 LOCAL_SRC_FILES := \
     CameraWrapper.cpp
 
 LOCAL_SHARED_LIBRARIES := \
-    libhardware liblog libcamera_client libutils android.hidl.token@1.0-utils libgui
+    libhardware liblog libcamera_client libutils android.hidl.token@1.0-utils libgui libhidltransport libsensor
 
 LOCAL_STATIC_LIBRARIES := \
     libarect libbase
