ba8fe6432196ba208cbf9a353c2444c193916cbb
RodrigoAyuso <rodrigo.mazzonetti@gmail.com>
2026-01-04 16:03:56 -0300

    victara: Remove libqsap_shim
    Moved to hardware/motorola

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -61,16 +61,6 @@ LOCAL_SHARED_LIBRARIES := \
     libsensor
 include $(BUILD_SHARED_LIBRARY)
 
-
-include $(CLEAR_VARS)
-LOCAL_SRC_FILES := libqsap_shim.c
-LOCAL_SHARED_LIBRARIES := libqsap_sdk liblog libcutils libutils
-LOCAL_C_INCLUDES := $(TOP)/system/qcom/softap/sdk
-LOCAL_MODULE := libqsap_shim
-LOCAL_MODULE_TAGS := optional
-LOCAL_PROPRIETARY_MODULE := true
-include $(BUILD_SHARED_LIBRARY)
-
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES := libaudioclient_shim.cpp
 LOCAL_MODULE := libaudioclient_shim
