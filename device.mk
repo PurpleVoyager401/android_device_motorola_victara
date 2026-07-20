87d2dd729838c5594bc428d95b43ee8f92f10561
PurpleVoyager401 <153445395+PurpleVoyager401@users.noreply.github.com>
2026-07-20 22:21:58 +0000

    [DNM] victara: Import HAL3in1 from device_lenovo_a6010
    Ref: https://github.com/acroreiser/android_device_lenovo_a6010/tree/lineage-23.2/camera/hal3on1
    
    It’s genuinely annoying to pick/revert/apply 985345882 patches to the ROM source (patches in frameworks/av and frameworks/base)—especially in future Android version bringups where those patches break—just to bring back Camera HAL1 functionality.
    Unfortunately, as a result, the Victara (Motorola Moto X2) will lose full support for the Camera2API, Support with this implementation is downgraded to LIMITED, but on the plus side, it will use Camera HAL3 (with the Camera HAL1 backend, eliminating the need to apply multiple patches).
    This is for the sake and sanity of those who are maintaining and will continue to maintain this device in the future.
    Credit and thanks to @acroreiser for the Hal3on1 implementation.
    
    Co-authored-by: Yaroslav Zviezda <acroreiser@gmail.com>
    
    Signed-off-by: PurpleVoyager401 <just.aenthusiast@gmail.com>

--- device.mk
+++ device.mk
@@ -69,6 +69,7 @@ TARGET_SCREEN_WIDTH := 1080
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     camera.device@1.0-impl \
+    camera.legacy.msm8974 \
     camera.msm8974 \
     libbson \
     libshim_camera \
